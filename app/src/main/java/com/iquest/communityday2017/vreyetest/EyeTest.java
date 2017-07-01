07242aaeebc91f3960af9f324264702d180c23f8
Andrei Hegedus-Sirbu <andrei.hegedus-sirbu@iquestgroup.com>
2017-07-01 15:23:08 +0300

    Added Eye Test Rows.

---
+++ app/src/main/java/com/iquest/communityday2017/vreyetest/EyeTest.java
@@ -0,0 +1,36 @@
+package com.iquest.communityday2017.vreyetest;
+
+/**
+ * @author andrei.hegedus
+ */
+
+public class EyeTest {
+
+    // http://www.dizziness-and-balance.com/practice/images/equipment/die%20chart.2.gif
+    private char[][] tests;
+
+    {
+        tests[0] = new char[]{'F', 'P'};
+        tests[1] = new char[]{'T', 'O', 'Z'};
+        tests[2] = new char[]{'L', 'P', 'E', 'D'};
+        tests[3] = new char[]{'P', 'E', 'C', 'F', 'D'};
+        tests[4] = new char[]{'E', 'D', 'F', 'C', 'Z', 'P'};
+        tests[5] = new char[]{'F', 'E', 'L', 'O', 'P', 'Z', 'D'};
+        tests[6] = new char[]{'D', 'E', 'F', 'P', 'O', 'T', 'E', 'C'};
+        tests[7] = new char[]{'L', 'E', 'F', 'O', 'D', 'P', 'C', 'T'};
+        tests[8] = new char[]{'F', 'D', 'P', 'L', 'T', 'C', 'E', 'O'};
+        tests[9] = new char[]{'P', 'E', 'Z', 'O', 'L', 'C', 'F', 'T', 'D'};
+    }
+
+    public char[] getRow(int nr) {
+        if (nr >= 0 && nr <= 9) {
+            return tests[nr];
+        } else {
+            throw new IllegalArgumentException("No such row: " + nr);
+        }
+    }
+
+    public String getRowAsString(int nr){
+        return new String(getRow(nr));
+    }
+}
